136b4cf90ed1418d4f355a6f1537ce3a5e535ffe
roberto oliveira <37985531+pedrashow@users.noreply.github.com>
2019-06-12 13:02:45 -0300

    Add files via upload

--- questao9.sql
+++ questao9.sql
@@ -4,6 +4,8 @@ WHERE a.co_estadocivil = 'C' AND a.co_aluno IN (SELECT f.co_aluno AS co_aluno
 FROM frequencia f
 INNER JOIN turma t ON t.co_turma = f.co_turma
 WHERE f.frequencia = 'S' AND t.ano = 2002
-GROUP BY f.co_aluno
+GROUP BY f.co_aluno, t.co_turma
 HAVING COUNT(f.frequencia) >= 8
-)
+) AND a.co_aluno NOT IN (SELECT DISTINCT alt.co_aluno FROM aluno_turma alt
+INNER JOIN turma t ON t.co_turma = alt.co_turma
+WHERE alt.dt_cancelamento IS NOT NULL AND t.ano = 2003)
