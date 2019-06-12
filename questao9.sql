3565ef1908cdcfa4ed4e86ba0d16d0f7a0e90fd4
roberto oliveira <37985531+pedrashow@users.noreply.github.com>
2019-06-12 12:41:32 -0300

    Add files via upload

--- questao9.sql
+++ questao9.sql
@@ -1,8 +1,9 @@
 SELECT distinct a.nome FROM aluno a
 INNER JOIN aluno_turma alt ON a.co_aluno = alt.co_aluno
-WHERE a.co_estadocivil = 'C' AND a.co_aluno IN (SELECT f.co_aluno AS co_aluno, COUNT(f.frequencia) AS presencas
+WHERE a.co_estadocivil = 'C' AND a.co_aluno IN (SELECT f.co_aluno AS co_aluno
 FROM frequencia f
-INNER JOIN turma t ON f.co_turma = t.co_turma
+INNER JOIN turma t ON t.co_turma = f.co_turma
 WHERE f.frequencia = 'S' AND t.ano = 2002
 GROUP BY f.co_aluno
+HAVING COUNT(f.frequencia) >= 8
 )
